92ad64ecbe3b224ecc5d7ea5cf5fb1afdeaa6761
kelvin <71570158+kelvin200458890@users.noreply.github.com>
2020-09-30 19:11:51 +0530

    StringToBinaryCompleted

---
+++ EncoderAndEncrypter/Models/Converter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncoderAndEncrypter.Models
+{
+    class Converter
+    {
+        /// <summary>
+        /// A more mathmatical approach to ASCII to Binary conversion
+        /// https://forums.asp.net/t/1713174.aspx?How+to+convert+ASCII+value+to+binary+value+using+c+net
+        /// </summary>
+        /// <param name="data">String to convert</param>
+        /// <returns>Binary encoded string</returns>
+        public static string StringToBinary2(string data)
+        {
+            string converted = string.Empty;
+            // convert string to byte array
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    converted += (bytes[i] & 0x80) > 0 ? "1" : "0";
+                    bytes[i] <<= 1;
+                }
+            }
+
+            return converted;
+        }
+    }
+}
---
+++ EncoderAndEncrypter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EncoderAndEncrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name: ");
+            var fullName = Console.ReadLine();
+            string binaryValue2 = Models.Converter.StringToBinary2(fullName);
+            Console.WriteLine($"Text: {fullName}\nBinary: {binaryValue2}");
+        }
+    }
+}
